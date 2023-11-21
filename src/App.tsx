465025c3db1a038421a9defa3c19158764f9f21b
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2023-11-21 16:29:46 +0000

    refactor: change to useeffect for data loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import jsonData from "../data.json";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Chart, AxisOptions } from "react-charts";
 import "./App.css";
 
@@ -13,17 +12,46 @@ type Series = {
   data: KRPoints[];
 };
 
-const data: Series[] = [
-  {
-    label: "Zombie_Area51",
-    data: [...jsonData].map((v) => ({
-      date: new Date(v.timestamp * 1000),
-      kr: v.KR,
-    })),
-  },
-];
+type Data = {
+  timestamp: number;
+  KR: number;
+};
 
 function App() {
+  const [data, setData] = useState<Series[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const raw = await fetch(
+          "https://raw.githubusercontent.com/alcpereira/kr-scraper/main/data.json",
+        );
+        const json: Data[] = await raw.json();
+        return json;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const formatData = async () => {
+      const jsonData = await fetchData();
+      if (!jsonData) return;
+
+      const formattedData = [
+        {
+          label: "Zombie_Area51",
+          data: [...jsonData].map((v) => ({
+            date: new Date(v.timestamp * 1000),
+            kr: v.KR,
+          })),
+        },
+      ];
+
+      setData(formattedData);
+    };
+    formatData();
+  }, []);
+
   const primaryAxis = useMemo(
     (): AxisOptions<KRPoints> => ({
       getValue: (datum) => datum.date,
@@ -42,15 +70,17 @@ function App() {
 
   return (
     <div className="chart-container">
-      <Chart
-        options={{
-          initialWidth: 800,
-          initialHeight: 400,
-          data,
-          primaryAxis,
-          secondaryAxes,
-        }}
-      />
+      {data.length && (
+        <Chart
+          options={{
+            initialWidth: 800,
+            initialHeight: 400,
+            data,
+            primaryAxis,
+            secondaryAxes,
+          }}
+        />
+      )}
     </div>
   );
 }
