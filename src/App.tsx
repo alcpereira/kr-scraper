624a05b3be2c9bf494ab69ba2448ec6afd994343
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2023-11-21 13:55:00 +0000

    feat: add chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
-import data from "../data.json";
+import jsonData from "../data.json";
+import { useMemo } from "react";
+import { Chart, AxisOptions } from "react-charts";
 import "./App.css";
 
+type KRPoints = {
+  date: Date;
+  kr: number;
+};
+
+type Series = {
+  label: string;
+  data: KRPoints[];
+};
+
+const data: Series[] = [
+  {
+    label: "Zombie_Area51",
+    data: [...jsonData].map((v) => ({
+      date: new Date(v.timestamp * 1000),
+      kr: v.KR,
+    })),
+  },
+];
+
 function App() {
-  return <main>{JSON.stringify(data)}</main>;
+  const primaryAxis = useMemo(
+    (): AxisOptions<KRPoints> => ({
+      getValue: (datum) => datum.date,
+    }),
+    [],
+  );
+
+  const secondaryAxes = useMemo(
+    (): AxisOptions<KRPoints>[] => [
+      {
+        getValue: (datum) => datum.kr,
+      },
+    ],
+    [],
+  );
+
+  return (
+    <div className="chart-container">
+      <Chart
+        options={{
+          initialWidth: 800,
+          initialHeight: 400,
+          data,
+          primaryAxis,
+          secondaryAxes,
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
